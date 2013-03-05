54486ccd7720ec7754c86ffe93fc7d7ebfa879f3
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-03-04 22:18:15 -0800

    setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+#-*- coding: utf-8 -*-
+
+"""
+    glassdoor
+    ~~~~~~~~~
+
+    Setup
+    `````
+
+    $ pip install glassdoor
+"""
+
+from distutils.core import setup
+
+setup(
+    name='glassdoor',
+    version='0.0.1',
+    url='http://github.com/hackerlist/glassdoor',
+    author='mek',
+    author_email='m@hackerlist.net',
+    packages=[
+        'glassdoor',
+        'test'
+        ],
+    platforms='any',
+    license='LICENSE',
+    install_requires=[
+        'requests >= 1.1.0',
+        'BeautifulSoup >= 3.2.1',
+    ],
+    scripts=[
+        "scripts/glassdoor"
+        ],
+    description="Glassdoor Python API",
+    long_description=open('README.md').read(),
+)
