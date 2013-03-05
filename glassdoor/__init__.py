8e285f7a5eda3c7da06e39425636193e075b8d5e
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-03-04 22:17:57 -0800

    scaffolding

---
+++ glassdoor/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Glassdoor Python API"""
+
+__version__ = "0.0.1"
+__author__ = [
+    "Mek <michael.karpeles@gmail.com>"
+]
+__license__ = "public domain"
+__contributors__ = "see AUTHORS"
+
+from glassdoor import GET
