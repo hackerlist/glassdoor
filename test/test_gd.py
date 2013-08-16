15a69ea73ebe5226ce2c8c444e8cacdcd374a960
Michael E. Karpeles <mek@jiffy>
2013-08-15 18:52:03 -0700

    fixed test case

--- test/test_gd.py
+++ test/test_gd.py
@@ -7,8 +7,8 @@ stdkeys = ['ceo', 'salary', 'satisfaction', 'connections']
 class TestGD(unittest.TestCase):
 
     def test_getjson(self):
+        gd = glassdoor.parse(glassdoor.get('dropbox'))
         try:
-            gd = glassdoor.GETjson('dropbox')
             self.assertTrue(all([k in gd.keys() for k in stdkeys]),
                             'Missing Keys')
         except Exception as e:
