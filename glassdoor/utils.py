cef72d38db92a5415c20d4d7f61a06cc9b7a5004
Michael E. Karpeles <michael.karpeles@gmail.com>
2014-01-27 17:55:21 -0800

    making utils.tryelse more verbose for debugging

--- glassdoor/utils.py
+++ glassdoor/utils.py
@@ -4,10 +4,12 @@ def intify(s):
     """Coerce string to int"""
     return int(re.sub("[^0-9]", "", s))
 
-def tryelse(func, default='', exception=Exception):
+def tryelse(func, default='', exception=Exception, log=False):
     """
     """
     try:
         return func()
     except exception as e:
+        if log:
+            print e
         return default
