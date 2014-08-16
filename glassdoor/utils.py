0cd1db56929beea2143e59d03777f254b740a7b0
Alexey Smirnov <alsmirn@gmail.com>
2014-08-16 19:52:23 +0400

    fix cleaning for salaries in russian currency

--- glassdoor/utils.py
+++ glassdoor/utils.py
@@ -2,6 +2,7 @@ import re
 
 def intify(s):
     """Coerce string to int"""
+    s = s.rstrip(' &#x0420;&#x0420;')
     return int(re.sub("[^0-9]", "", s))
 
 def tryelse(func, default='', exception=Exception, log=False):
